66aa06b31f088ebd81cce951935518da8e4e8cce
deepthings <thingsdeep07@gmail.com>
2024-06-15 20:38:23 +0530

    typo

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -113,7 +113,7 @@ const loginUser = asyncHandler( async (req , res) => {
     //send cookie
 
     const {email,username,password} = req.body
-    if (!username || !email) {
+    if (!username && !email) {
         throw new ApiError(400, "username or email is required")
     }
 
@@ -128,7 +128,7 @@ const loginUser = asyncHandler( async (req , res) => {
     if(!isPasswordValid){
         throw new ApiError(401 , "password Incorrect")
     }    
-    const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id)
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
 
 
     //optional step 28:00 
